2cb7db8d7cfca89d4de48070f0534ae5090fccf3
正版青尘 <201436728@qq.com>
2025-07-06 03:14:21 +0800

    feat: iOS端实现PAGAnimation

--- library/src/iosMain/kotlin/love/yinlin/libpag/PAGAnimation.kt
+++ library/src/iosMain/kotlin/love/yinlin/libpag/PAGAnimation.kt
@@ -14,7 +14,36 @@ import org.jetbrains.skia.Image
 import org.jetbrains.skia.ImageInfo
 import platform.CoreGraphics.*
 import platform.CoreVideo.*
+import platform.darwin.NSObject
 import platform.posix.memcpy
+import kotlin.math.min
+
+@OptIn(ExperimentalForeignApi::class)
+fun PAGFile.Companion.Load(data: ByteArray) = data.usePinned {
+    PAGFile.Load(it.addressOf(0), it.get().size.toULong())
+}
+
+@OptIn(ExperimentalForeignApi::class)
+fun IntSize.toCGSize() = CGSizeMake(width.toDouble(), height.toDouble())
+
+@OptIn(ExperimentalForeignApi::class)
+private class PAGViewListenerImpl(val listener: PAGConfig.AnimationListener) : NSObject(), PAGViewListenerProtocol {
+    override fun onAnimationStart(v: PAGView?) = listener.onAnimationStart(v)
+    override fun onAnimationEnd(v: PAGView?) = listener.onAnimationEnd(v)
+    override fun onAnimationCancel(v: PAGView?) = listener.onAnimationCancel(v)
+    override fun onAnimationRepeat(v: PAGView?) = listener.onAnimationRepeat(v)
+    override fun onAnimationUpdate(v: PAGView?) = listener.onAnimationUpdate(v, v?.getProgress() ?: 0.0)
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private class PAGImageViewListenerImpl(val listener: PAGConfig.AnimationListener) : NSObject(), PAGImageViewListenerProtocol {
+    override fun onAnimationStart(v: PAGImageView) = listener.onAnimationStart(v)
+    override fun onAnimationEnd(v: PAGImageView) = listener.onAnimationEnd(v)
+    override fun onAnimationCancel(v: PAGImageView) = listener.onAnimationCancel(v)
+    override fun onAnimationRepeat(v: PAGImageView) = listener.onAnimationRepeat(v)
+    override fun onAnimationUpdate(v: PAGImageView) =
+        listener.onAnimationUpdate(v, v.currentFrame().toDouble() / v.numFrames().toDouble())
+}
 
 @OptIn(ExperimentalForeignApi::class)
 fun createImageFromPixelBuffer(pixelBuffer: CVPixelBufferRef): Image? {
@@ -42,6 +71,7 @@ fun createImageFromPixelBuffer(pixelBuffer: CVPixelBufferRef): Image? {
     )
 }
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun PAGAnimation(
     data: ByteArray?,
@@ -53,12 +83,15 @@ actual fun PAGAnimation(
     listener: PAGConfig.AnimationListener,
 ) {
     val pagView = remember { mutableStateOf<PAGView?>(null) }
+    val pagViewListener: MutableState<PAGViewListenerImpl> =
+        remember { mutableStateOf(PAGViewListenerImpl(listener)) }
 
     UIKitView(
         modifier = modifier,
         factory = {
             pagView.value ?: PAGView().apply {
                 pagView.value = this
+                addListener(pagViewListener.value)
             }
         },
         update = { view ->
@@ -71,6 +104,49 @@ actual fun PAGAnimation(
             })
         }
     )
+
+    LaunchedEffect(data) {
+        pagView.value?.let { view ->
+            view.setComposition(if (data != null) PAGFile.Load(data) else null)
+        }
+    }
+
+    LaunchedEffect(isPlaying) {
+        pagView.value?.let { view ->
+            if (isPlaying) view.play()
+            else view.pause()
+        }
+    }
+
+    LaunchedEffect(progress) {
+        pagView.value?.let { view ->
+            view.setProgress(progress)
+            view.flush()
+        }
+    }
+
+    LaunchedEffect(repeatCount, scaleMode) {
+        pagView.value?.let { view ->
+            view.setRepeatCount(repeatCount)
+            view.setScaleMode(when (scaleMode) {
+                PAGConfig.ScaleMode.None -> PAGScaleModeNone
+                PAGConfig.ScaleMode.Stretch -> PAGScaleModeStretch
+                PAGConfig.ScaleMode.LetterBox -> PAGScaleModeLetterBox
+                PAGConfig.ScaleMode.Zoom -> PAGScaleModeZoom
+            })
+        }
+    }
+
+    LaunchedEffect(listener) {
+        if (listener == pagViewListener.value.listener) {
+            return@LaunchedEffect
+        }
+        pagView.value?.let { view ->
+            view.removeListener(pagViewListener.value)
+            pagViewListener.value = PAGViewListenerImpl(listener)
+            view.addListener(pagViewListener.value)
+        }
+    }
 }
 
 @OptIn(ExperimentalForeignApi::class)
@@ -86,26 +162,27 @@ actual fun PAGImageAnimation(
     listener: PAGConfig.AnimationListener,
 ) {
     val pagImageView = remember { mutableStateOf<PAGImageView?>(null) }
+    val pagImageViewListener: MutableState<PAGImageViewListenerImpl> =
+        remember { mutableStateOf(PAGImageViewListenerImpl(listener)) }
 
     UIKitView(
         modifier = modifier,
         factory = {
             pagImageView.value ?: PAGImageView().apply {
                 pagImageView.value = this
+                addListener(pagImageViewListener.value)
             }
         },
         update = { view ->
             view.setRepeatCount(repeatCount)
+            view.setRenderScale(renderScale)
             view.setCacheAllFramesInMemory(cacheAllFramesInMemory)
         }
     )
 
     LaunchedEffect(data) {
         pagImageView.value?.let { view ->
-            val pagFile = data?.usePinned {
-                PAGFile.Load(it.addressOf(0), it.get().size.toULong())
-            }
-            view.setComposition(pagFile)
+            view.setComposition(if (data != null) PAGFile.Load(data) else null)
         }
     }
 
@@ -118,8 +195,27 @@ actual fun PAGImageAnimation(
 
     LaunchedEffect(progress) {
         pagImageView.value?.let { view ->
-            view.setCurrentFrame((progress * view.numFrames().toDouble()).toULong())
+            view.setCurrentFrame(min((progress * view.numFrames().toInt()).toULong(), view.numFrames()))
             view.flush()
+        }
+    }
+
+    LaunchedEffect(repeatCount, renderScale, cacheAllFramesInMemory) {
+        pagImageView.value?.let { view ->
+            view.setRepeatCount(repeatCount)
+            view.setRenderScale(renderScale)
+            view.setCacheAllFramesInMemory(cacheAllFramesInMemory)
+        }
+    }
+
+    LaunchedEffect(listener) {
+        if (listener == pagImageViewListener.value.listener) {
+            return@LaunchedEffect
+        }
+        pagImageView.value?.let { view ->
+            view.removeListener(pagImageViewListener.value)
+            pagImageViewListener.value = PAGImageViewListenerImpl(listener)
+            view.addListener(pagImageViewListener.value)
         }
     }
 }
@@ -136,14 +232,12 @@ actual fun rememberPAGPainter(
     val painter = remember { mutableStateOf<Painter>(BitmapPainter(ImageBitmap(1, 1))) }
 
     LaunchedEffect(data) {
-        data?.usePinned {
-            PAGFile.Load(it.addressOf(0), it.get().size.toULong())
-        }?.let { pagFile ->
+        if (data == null) return@LaunchedEffect
+        PAGFile.Load(data)?.let { pagFile ->
             player.setComposition(pagFile)
-            val width = pagFile.width().toDouble()
-            val height = pagFile.height().toDouble()
-//            surface.value?.release()
-            surface.value = PAGSurface.MakeOffscreen(CGSizeMake(width, height))
+            val size = if (size == IntSize.Zero) IntSize(pagFile.width().toInt(), pagFile.height().toInt()) else size
+//            surface.value?.objcPtr()?.let { objc_release(it) }
+            surface.value = PAGSurface.MakeOffscreen(size.toCGSize())
             surface.value?.let { surface ->
                 player.setSurface(surface)
             }
@@ -160,12 +254,13 @@ actual fun rememberPAGPainter(
         }
     }
 
-    DisposableEffect(player, surface.value) {
-        onDispose {
-//            surface.value?.release()
-//            player.release()
-        }
-    }
+    // DisposableEffect(Unit) {
+    //     onDispose {
+    //         surface.value?.objcPtr()?.let { objc_release(it) }
+    //         surface.value = null
+    //         objc_release(player.objcPtr())
+    //     }
+    // }
 
     return painter.value
 }
